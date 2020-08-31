377a02c71294d3d5adb90f796009ea236917bfec
Xiaojue Tian <xiatia@microsoft.com>
2020-08-31 16:01:25 +0800

    2020.08.31 added more c-cda sample data

--- src/e2e-test/regression-test/config/testcases-cda.js
+++ src/e2e-test/regression-test/config/testcases-cda.js
@@ -3,13 +3,19 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-const dataFiles = [ 'C-CDA_R2-1_CCD.xml.cda', '170.314B2_Amb_CCD.cda', 'Patient-1.cda', 'sample.cda' ];
+const dataFiles = [
+    '170.314B2_Amb_CCD.cda', 'C-CDA_R2-1_CCD.xml.cda', 'Care_Plan.cda', 'CCD-Parent-Document-Replace-C-CDAR2.1.cda',
+    'CCD.cda', 'CDA_with_Embedded_PDF.cda', 'Consult-Document-Closing-Referral-C-CDAR2.1.cda', 'Consultation_Note.cda',
+    'Diagnostic_Imaging_Report.cda', 'Discharge_Summary.cda', 'History_and_Physical.cda', 'Operative_Note.cda',
+    'Patient-1.cda', 'Patient-and-Provider-Organization-Direct-Address-C-CDAR2.1.cda', 'PROBLEMS_in_Empty_C-CDA_2.1-C-CDAR2.1.cda',
+    'Procedure_Note.cda', 'Progress_Note.cda', 'Referral_Note.cda', 'sample.cda', 'Transfer_Summary.cda',
+    'Unstructured_Document_embed.cda', 'Unstructured_Document_reference.cda' ];
 const templateFiles = [ 'ccd.hbs' ];
 
 module.exports = () => {
     const cases = [ ];
     dataFiles.forEach(dataFile => cases.push(
-        ...templateFiles.map(templateFile => 
+        ...templateFiles.map(templateFile =>
             ({ dataFile, templateFile }))));
     return cases;
 };
