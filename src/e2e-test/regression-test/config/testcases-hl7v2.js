efe749786aaf79f7f072134c459e05f9466b812b
Xiaojue Tian <xiatia@microsoft.com>
2020-09-03 16:53:39 +0800

    2020.09.03 added a set of HL7V2 messages and ground truths (mainly about OML_O21)

--- src/e2e-test/regression-test/config/testcases-hl7v2.js
+++ src/e2e-test/regression-test/config/testcases-hl7v2.js
@@ -4,19 +4,20 @@
 // -------------------------------------------------------------------------------------------------
 
 const patterns = [
-    { template: 'ADT_A01.hbs', data: [ 'ADT01-23.hl7', 'ADT01-28.hl7' ] },
-    { template: 'VXU_V04.hbs', data: [ 'VXU.hl7' ] },
-    { template: 'ORU_R01.hbs', data: [
+    { templateFile: 'ADT_A01.hbs', dataFiles: [ 'ADT01-23.hl7', 'ADT01-28.hl7' ] },
+    { templateFile: 'VXU_V04.hbs', dataFiles: [ 'VXU.hl7' ] },
+    { templateFile: 'ORU_R01.hbs', dataFiles: [
         'LAB-ORU-1.hl7', 'LAB-ORU-2.hl7', 'LRI_2.0-NG_CBC_Typ_Message.hl7', 'EKG-LAB-ORU-R01-1.hl7',
         'EKG-LAB-ORU-R01-4.hl7', 'GOOD-LAD-ORU-R01-1.hl7','ORU-R01-RMGEAD.hl7'
-    ] }
+    ]},
+    { templateFile: 'OML_O21.hbs', dataFiles: [ 'MDHHS-OML-O21-1.hl7', 'MDHHS-OML-O21-2.hl7' ] }
 ];
 
 module.exports = () => {
     const cases = [ ];
     patterns.forEach(pattern => {
-        for (const dataFile of pattern.data) {
-            cases.push({ templateFile: pattern.template, dataFile });
+        for (const dataFile of pattern.dataFiles) {
+            cases.push({ templateFile: pattern.templateFile, dataFile });
         }
     });
     return cases;
