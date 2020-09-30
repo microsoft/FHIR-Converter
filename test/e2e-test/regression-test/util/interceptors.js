719bca8c3513272e1ccae72912a3d2126fc38b2e
Ferris Tien <Ferris Tien's Microsoft Email Address>
2020-09-30 13:32:47 +0800

    2020.09.30 updated interceptor to filter platform-related fields

--- test/e2e-test/regression-test/util/interceptors.js
+++ test/e2e-test/regression-test/util/interceptors.js
@@ -24,6 +24,12 @@ class DoNothingInterceptor extends Interceptor {
 }
 
 class ExtraDynamicFieldInterceptor extends Interceptor {
+    constructor (next) {
+        super(next);
+        this.__placeholder = 'removed';
+        this.__removeUUIDResourceTypes = [ 'DocumentReference', 'Composition', 'Immunization', 'MedicationStatement' ];
+    }
+
     handle (data) {
         if (!_.isPlainObject(data)) {
             return data;
@@ -33,58 +39,78 @@ class ExtraDynamicFieldInterceptor extends Interceptor {
     }
 
     __handle (data) {
-        if (!('entry' in data)) {
+        if (!('entry' in data) || !_.isArray(data['entry'])) {
             return data;
         }
-
         const entries = data['entry'];
-        const placeholder = 'removed';
-        const removeUUIDResourceTypes = [ 'DocumentReference', 'Composition' ];
 
-        if (!_.isArray(entries)) {
-            return data;
-        }
         for (const entry of entries) {
             if (!entry || !('resource' in entry) || !('resourceType' in entry['resource'])) {
                 continue;
             }
 
             const resource = entry['resource'];
-            const request = ('request' in entry) ? entry['request'] : null;
+            this.__removeDocumentReference(resource);
+            this.__removeSectionReferences(resource);
+            this.__removeResourceIds(resource, entry);
+        }
+        return data;
+    }
 
-            if (resource['resourceType'] === 'DocumentReference') {
-                resource['date'] = placeholder;
+    __removeDocumentReference(resource) {
+        if (resource['resourceType'] != 'DocumentReference') {
+            return;
+        }
+        resource['date'] = this.__placeholder;
 
-                // The zlib.gzip result will be different on different platforms, see https://stackoverflow.com/questions/26516369/zlib-gzip-produces-different-results-for-same-input-on-different-oses.
-                // Hence the hash result will be different too, which will trigger NodeJS CI error and need to be removed.
-                if (('content' in resource) && _.isArray(resource['content'])) {
-                    for (const ele of resource['content']) {
-                        if ('attachment' in ele) {
-                            if ('hash' in ele['attachment']) {
-                                ele['attachment']['hash'] = 'removed-hash';
-                            }
-                            if ('data' in ele['attachment']) {
-                                ele['attachment']['data'] = 'removed-data';
-                            }
-                        }
-                    }
+        // The zlib.gzip result will be different on different platforms, see https://stackoverflow.com/questions/26516369/zlib-gzip-produces-different-results-for-same-input-on-different-oses.
+        // Hence the hash result will be different too, which will trigger NodeJS CI error and need to be removed.
+
+        if (!('content' in resource) || !_.isArray(resource['content'])) {
+            return;
+        }
+        for (const ele of resource['content']) {
+            if ('attachment' in ele) {
+                if ('hash' in ele['attachment']) {
+                    ele['attachment']['hash'] = 'removed-hash';
                 }
-            }
-
-            for (const rmUUIDType of removeUUIDResourceTypes) {
-                if (resource['resourceType'] !== rmUUIDType) {
-                    continue;
-                }
-
-                entry['fullUrl'] = placeholder;
-                resource['id'] = placeholder;
-
-                if (request && ('url' in request)) {
-                    request['url'] = placeholder;
+                if ('data' in ele['attachment']) {
+                    ele['attachment']['data'] = 'removed-data';
                 }
             }
         }
-        return data;
+    }
+
+    __removeSectionReferences(resource) {
+        if (!('section' in resource)) {
+            return;
+        }
+        for (const section of resource['section']) {
+            if ('entry' in section) {
+                for (let i = 0; i < section['entry'].length; ++ i) {
+                    const item = section['entry'][i];
+                    if ('reference' in item && this.__removeUUIDResourceTypes.some(e => item['reference'].includes(e))) {
+                        section['entry'][i] = this.__placeholder;
+                    }
+                }
+            }
+        }
+    }
+
+    __removeResourceIds(resource, entry) {
+        const request = ('request' in entry) ? entry['request'] : null;
+        for (const rmUUIDType of this.__removeUUIDResourceTypes) {
+            if (resource['resourceType'] != rmUUIDType) {
+                continue;
+            }
+
+            entry['fullUrl'] = this.__placeholder;
+            resource['id'] = this.__placeholder;
+
+            if (request && ('url' in request)) {
+                request['url'] = this.__placeholder;
+            }
+        }
     }
 }
 
