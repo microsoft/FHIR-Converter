b53cde3e93712bfbc81c36a96850ebb5128bef33
Ferris Tien <9417900+ACMoretxj@users.noreply.github.com>
2020-11-18 10:10:34 +0800

    2020.11.17 updated data and test configs (#99)
    * 2020.11.17 updated data and test configs
    
    * 2020.11.17 updated patient resource, added each block for repeatable identifier NK1-5
    
    * 2020.11.17 updated patient resource, added each block for repeatable identifier NK1-5
    
    * 2020.11.17 updated test cases. ***Important: the line ending in 'IZ_1_1.1_Admin_Child_Max_Message' must be LF, because there are many '^M' substring inside it, which will be incorrectly understanded as a windows-style line ending character*

--- test/e2e-test/rule-test/config/testcases-cda.js
+++ test/e2e-test/rule-test/config/testcases-cda.js
@@ -5,12 +5,20 @@
 
 // design the test cases
 var ruleSets = require('./rule-sets'); 
-var cdaTestcases = [
-    { dataFile: 'C-CDA_R2-1_CCD.xml.cda', templateFile: 'ccd.hbs', testRules: ruleSets.guidRules},
-    { dataFile: '170.314B2_Amb_CCD.cda', templateFile: 'ccd.hbs', testRules: ruleSets.commonRules},
-    { dataFile: 'Patient-1.cda', templateFile: 'ccd.hbs', testRules: ruleSets.commonRules},
-    { dataFile: 'sample.cda', templateFile: 'ccd.hbs', testRules: ruleSets.commonRules}
+
+const dataFiles = [
+    '170.314B2_Amb_CCD.cda', 'Care_Plan.cda', 'CCD.cda', 'C-CDA_R2-1_CCD.xml.cda', 'CCD-Parent-Document-Replace-C-CDAR2.1.cda',
+    'CDA_with_Embedded_PDF.cda', 'Consultation_Note.cda', 'Consult-Document-Closing-Referral-C-CDAR2.1.cda', 'Diagnostic_Imaging_Report.cda',
+    'Discharge_Summary.cda', 'History_and_Physical.cda', 'Operative_Note.cda', 'Patient-1.cda', 'Patient-and-Provider-Organization-Direct-Address-C-CDAR2.1.cda',
+    'PROBLEMS_in_Empty_C-CDA_2.1-C-CDAR2.1.cda', 'Procedure_Note.cda', 'Progress_Note.cda', 'Referral_Note.cda', 'sample.cda',
+    'Transfer_Summary.cda', 'Unstructured_Document_embed.cda', 'Unstructured_Document_reference.cda'
 ];
+const templateFiles = [ 'ccd.hbs' ];
 
-module.exports = cdaTestcases;
-
+module.exports = () => {
+    const cases = [ ];
+    dataFiles.forEach(dataFile => cases.push(
+        ...templateFiles.map(templateFile => 
+            ({ dataFile, templateFile, testRules: ruleSets.commonRules }))));
+    return cases;
+};
