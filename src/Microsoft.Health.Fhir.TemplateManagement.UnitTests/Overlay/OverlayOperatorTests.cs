ff0598e65593653811c063bea8fccafd652297fc
songrui <sowu@microsoft.com>
2020-11-30 21:41:24 +0800

    update tests

--- src/Microsoft.Health.Fhir.TemplateManagement.UnitTests/Overlay/OverlayOperatorTests.cs
+++ src/Microsoft.Health.Fhir.TemplateManagement.UnitTests/Overlay/OverlayOperatorTests.cs
@@ -140,7 +140,7 @@ namespace Microsoft.Health.Fhir.TemplateManagement.UnitTests.Overlay
             var diffLayers = _overlayOperator.GenerateDiffLayer(fileLayer, baseFileLayer);
 
             File.Delete("TestData/UserFolder/.image/base/defaultlayer.tar.gz");
-            Assert.Equal(814, diffLayers.FileContent.Count());
+            Assert.Equal(818, diffLayers.FileContent.Count());
             Assert.Equal(2, diffLayers.SequenceNumber);
         }
 
