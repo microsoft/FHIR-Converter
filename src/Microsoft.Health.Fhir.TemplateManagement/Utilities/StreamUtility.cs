b5fe89f34e310348f5c422b69b7b0a79e398814f
songrui <sowu@microsoft.com>
2021-09-01 14:22:50 +0800

    update

--- src/Microsoft.Health.Fhir.TemplateManagement/Utilities/StreamUtility.cs
+++ src/Microsoft.Health.Fhir.TemplateManagement/Utilities/StreamUtility.cs
@@ -57,6 +57,81 @@ namespace Microsoft.Health.Fhir.TemplateManagement.Utilities
             return artifacts;
         }
 
+        public void TarCreateFromStream()
+        {
+            // Create an output stream. Does not have to be disk, could be MemoryStream etc.
+            string tarOutFn = @"c:\temp\test.tar";
+            Stream outStream = File.Create(tarOutFn);
+
+            // If you wish to create a .Tar.GZ (.tgz):
+            // - set the filename above to a ".tar.gz",
+            // - create a GZipOutputStream here
+            // - change "new TarOutputStream(outStream)" to "new TarOutputStream(gzoStream)"
+            // Stream gzoStream = new GZipOutputStream(outStream);
+            // gzoStream.SetLevel(3); // 1 - 9, 1 is best speed, 9 is best compression
+
+            GZipOutputStream gzipOutputStream = new GZipOutputStream(outStream);
+            TarOutputStream tarOutputStream = new TarOutputStream(gzipOutputStream);
+
+            gzipOutputStream.SetLevel(3);
+
+            CreateTarManually(tarOutputStream, @"c:\temp\debug");
+
+            // Closing the archive also closes the underlying stream.
+            // If you don't want this (e.g. writing to memorystream), set tarOutputStream.IsStreamOwner = false
+            tarOutputStream.Close();
+        }
+
+        private void CreateTarManually(TarOutputStream tarOutputStream, string sourceDirectory)
+        {
+            // Optionally, write an entry for the directory itself.
+            TarEntry tarEntry = TarEntry.CreateEntryFromFile(sourceDirectory);
+            tarOutputStream.PutNextEntry(tarEntry);
+
+            // Write each file to the tar.
+            string[] filenames = Directory.GetFiles(sourceDirectory);
+
+            foreach (string filename in filenames)
+            {
+                // You might replace these 3 lines with your own stream code
+
+                using (Stream inputStream = File.OpenRead(filename))
+                {
+                    string tarName = filename.Substring(3); // strip off "C:\"
+
+                    long fileSize = inputStream.Length;
+
+                    // Create a tar entry named as appropriate. You can set the name to anything,
+                    // but avoid names starting with drive or UNC.
+                    TarEntry entry = TarEntry.CreateTarEntry(tarName);
+
+                    // Must set size, otherwise TarOutputStream will fail when output exceeds.
+                    entry.Size = fileSize;
+
+                    // Add the entry to the tar stream, before writing the data.
+                    tarOutputStream.PutNextEntry(entry);
+
+                    // this is copied from TarArchive.WriteEntryCore
+                    byte[] localBuffer = new byte[32 * 1024];
+                    while (true)
+                    {
+                        int numRead = inputStream.Read(localBuffer, 0, localBuffer.Length);
+                        if (numRead <= 0)
+                            break;
+
+                        tarOutputStream.Write(localBuffer, 0, numRead);
+                    }
+                }
+                tarOutputStream.CloseEntry();
+            }
+
+            // Recurse. Delete this if unwanted.
+
+            string[] directories = Directory.GetDirectories(sourceDirectory);
+            foreach (string directory in directories)
+                CreateTarManually(tarOutputStream, directory);
+        }
+
         private static byte[] ExtractEntry(TarInputStream tarIn)
         {
             using var outputStream = new MemoryStream();
