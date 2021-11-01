3b238500758a852039798bd27627b612b3556951
shashanknarang27 <shashank.narang@xoriant.com>
2021-11-01 13:20:21 +0530

    Test cases updated for Milestone-2&3

--- src/Microsoft.Health.Fhir.TemplateManagement.UnitTests/Providers/TemplateCollectionProviderTests.cs
+++ src/Microsoft.Health.Fhir.TemplateManagement.UnitTests/Providers/TemplateCollectionProviderTests.cs
@@ -85,8 +85,8 @@ namespace Microsoft.Health.Fhir.TemplateManagement.UnitTests.Providers
 
         public static IEnumerable<object[]> GetDefaultTemplatesInfo()
         {
-            yield return new object[] { "microsofthealth/fhirconverter:default", 827 };
-            yield return new object[] { "microsofthealth/hl7v2templates:default", 827 };
+            yield return new object[] { "microsofthealth/fhirconverter:default", 852 };
+            yield return new object[] { "microsofthealth/hl7v2templates:default", 852 };
             yield return new object[] { "microsofthealth/ccdatemplates:default", 821 };
             yield return new object[] { "microsofthealth/jsontemplates:default", 2 };
         }
