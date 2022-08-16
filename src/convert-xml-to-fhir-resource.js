e51901d30a301ba35e407cf8b323cd7cee4200e6
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-16 11:59:35 -0400

    Refactoring myVar to filePath

--- src/convert-xml-to-fhir-resource.js
+++ src/convert-xml-to-fhir-resource.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const WorkerPool = require("./lib/workers/workerPool");
 
 function createAndUploadFhirResource()  {
-    if(process.env.npm_config_myVar === '' || process.env.npm_config_myVar === null || process.env.npm_config_myVar === undefined) {
+    if(process.env.npm_config_filePath === '' || process.env.npm_config_filePath === null || process.env.npm_config_filePath === undefined) {
         console.log("Must specify file path");
     } else {
-        let filePath = "../../" + process.env.npm_config_myVar;
+        let filePath = "../../" + process.env.npm_config_filePath;
         const xmlFile = fs.readFileSync(filePath, 'utf8');
         const workerPool = new WorkerPool('./src/lib/workers/worker.js', require('os').cpus().length);
         return workerPool.exec({
