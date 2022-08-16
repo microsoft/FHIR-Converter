c93c74a1fa3cce35cc9195e50fc82d24709bfba8
Alyssa Gardiner <agardiner@onemedical.com>
2022-08-16 11:56:14 -0400

    Fixing file path to be tmp folder in staff

--- src/convert-xml-to-fhir-resource.js
+++ src/convert-xml-to-fhir-resource.js
@@ -5,7 +5,8 @@ function createAndUploadFhirResource()  {
     if(process.env.npm_config_myVar === '' || process.env.npm_config_myVar === null || process.env.npm_config_myVar === undefined) {
         console.log("Must specify file path");
     } else {
-        const xmlFile = fs.readFileSync(process.env.npm_config_myVar, 'utf8');
+        let filePath = "../../" + process.env.npm_config_myVar;
+        const xmlFile = fs.readFileSync(filePath, 'utf8');
         const workerPool = new WorkerPool('./src/lib/workers/worker.js', require('os').cpus().length);
         return workerPool.exec({
             'type': '/api/convert/:srcDataType/:template',
@@ -14,9 +15,9 @@ function createAndUploadFhirResource()  {
             'templateName': "ccd.hbs"
         }).then((result) => {
             const resultMessage = result.resultMsg;
-            let newPath = process.env.npm_config_myVar.slice(0, -4);
-            newPath += "--FHIR-RESOURCE.json";
-            fs.writeFileSync(newPath, JSON.stringify(resultMessage));
+            let newPath = filePath.replace(".xml", "--FHIR-RESOURCE.json");
+            newPath = newPath.split("tmp").pop();
+            fs.writeFileSync("../../tmp/" + newPath, JSON.stringify(resultMessage));
         }).then(() => {
             workerPool.destroy();
         });
