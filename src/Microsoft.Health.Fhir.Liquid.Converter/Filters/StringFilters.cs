a1ac10e3d4fed532c58558aa193068ed9e2cb0af
Luke Forehand <luke@mergencehealth.com>
2025-02-17 10:24:37 -0600

    remove unused code

--- src/Microsoft.Health.Fhir.Liquid.Converter/Filters/StringFilters.cs
+++ src/Microsoft.Health.Fhir.Liquid.Converter/Filters/StringFilters.cs
@@ -88,9 +88,9 @@ namespace Microsoft.Health.Fhir.Liquid.Converter
 
         public static string Sha1Hash(string data)
         {
-#pragma warning disable CA5350
+#pragma warning disable SYSLIB0021
             using var sha1 = new SHA1Managed(); // lgtm[cs/weak-crypto]
-#pragma warning restore CA5350
+#pragma warning restore SYSLIB0021
             var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(data));
             return string.Concat(hash.Select(b => b.ToString("x2")));
         }
